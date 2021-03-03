9243082ad1abcfd02853955b92446d3f69338c85
Anmol Verma <anmol.verma@mutualmobile.com>
2021-03-03 17:01:24 +0530

    setup useTransportSecurity with local certs

--- src/main/kotlin/me/anmolverma/MainGrpcServer.kt
+++ src/main/kotlin/me/anmolverma/MainGrpcServer.kt
@@ -1,16 +1,10 @@
 package me.anmolverma
 
-import io.grpc.ServerBuilder
 import me.anmolverma.auth.JWTInterceptor
 import me.anmolverma.categories.data.InMemoryCategoryDataSource
 import me.anmolverma.products.data.InMemoryProductsDataSource
 import me.anmolverma.services.KartProductsService
-import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts
 import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder
-
-import io.grpc.netty.shaded.io.netty.handler.ssl.ClientAuth
-
-import io.grpc.netty.shaded.io.netty.handler.ssl.SslContextBuilder
 import java.io.File
 
 
@@ -21,13 +15,8 @@ object MainGrpcServer {
         try {
             val server = NettyServerBuilder
                 .forPort(8443)
-                .sslContext(
-                    getSslContextBuilder(
-                        certChainFilePath = args[0],
-                        privateKeyFilePath = args[1],
-                        trustCertCollectionFilePath = args[2]
-                    ).build()
-                )
+                .useTransportSecurity(File(System.getenv("certChainFilePath")),
+                    File( System.getenv("privateKeyFilePath")))
                 .addService(
                     KartProductsService(
                         InMemoryProductsDataSource(),
@@ -44,19 +33,4 @@ object MainGrpcServer {
         }
     }
 
-    private fun getSslContextBuilder(
-        certChainFilePath: String,
-        privateKeyFilePath: String,
-        trustCertCollectionFilePath: String?
-    ): SslContextBuilder {
-        val sslClientContextBuilder: SslContextBuilder = SslContextBuilder.forServer(
-            File(certChainFilePath),
-            File(privateKeyFilePath)
-        )
-        trustCertCollectionFilePath?.let {
-            sslClientContextBuilder.trustManager(File(trustCertCollectionFilePath))
-            sslClientContextBuilder.clientAuth(ClientAuth.REQUIRE)
-        }
-        return GrpcSslContexts.configure(sslClientContextBuilder)
-    }
 }
